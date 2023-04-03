605933d34d6c9e593ecf9830704334292baa372b
Douglas Pinheiro <dgspramos@gmail.com>
2023-04-02 21:51:44 -0300

    Teste

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <>
     <div className="relative h-full w-full bg-[url('../public/images/ia.png')] bg-no-repeat bg-center bg-fixed bg-cover">
     <h1 className="text-2xl text-purple-500">Dgsflix Next 13 2023 and Deploy Heroku <a href="./auth">Acesse aqui</a></h1>
-    <h1>Teste developer deploy</h1>
+    <h1 className="text-2xl text-blue-500">Teste developer deploy</h1>
     </div>
       
      
