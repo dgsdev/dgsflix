036cb021b97cb56fcf47f5d6b8be35c73b6abd49
Douglas Pinheiro <dgspramos@gmail.com>
2023-04-04 01:12:51 -0300

    Red Hat OpenShift Dedicado na AWS ROSA

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,10 @@ export default function Home() {
   return (
     <>
     <div className="relative h-full w-full bg-[url('../public/images/ia.png')] bg-no-repeat bg-center bg-fixed bg-cover">
-    <h1 className="text-2xl text-purple-500">Dgsflix Next 13 2023 and Deploy Heroku <a href="./auth">Acesse aqui</a></h1>
-    <h1 className="text-2xl text-blue-500">Teste developer deploy</h1>
+    <h1 className="text-2xl text-purple-500">Dgsflix NextJS 13 2023 com Deploy dynos do Heroku </h1>
+    <h2 className="text-2xl text-yellow-500"><a href="./auth">Acesse aqui</a></h2>
+    <h1 className="text-2xl text-blue-500">Teste deploy AWS Amplify</h1>
+    <h1 className="text-2xl text-green-400">Deploy pod Red Hat OpenShift Dedicado na AWS</h1>
     </div>
       
      
