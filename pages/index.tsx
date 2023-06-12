a89dd553ca4ce55b9d7dbfa309d7833f4cfddd9b
Douglas P Ramos <dgspramos@gmail.com>
2023-06-12 19:21:18 -0300

    teste

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,8 @@ export default function Home() {
     <div className="relative h-full w-full bg-[url('../public/images/ia.png')] bg-no-repeat bg-center bg-fixed bg-cover">
     <h1 className="text-2xl text-purple-500">Dgsflix NextJS 13 2023 com Deploy dynos do Heroku </h1>
     <h2 className="text-2xl text-yellow-500"><a href="./auth">Acesse aqui</a></h2>
-    <h1 className="text-2xl text-blue-500">Teste deploy AWS Amplify</h1>
-    <h1 className="text-2xl text-green-400">Deploy Red Hat OpenShift Dedicado na AWS</h1>
-    <h1 className="text-2xl text-yellow-500">Teste Webhook</h1>
+    <h1 className="text-2xl text-blue-500">Teste deploy AWS Amplify</h1> 
+    
     </div>
       
      
